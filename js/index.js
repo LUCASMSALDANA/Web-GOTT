f3418aa57767a2b3ffc089704d1cae6e43c28242
Lucas <lucasmsaldana@gmail.com>
2023-02-19 21:41:18 -0300

    HEADER: Creamos el Nav AreaMarket: trabajamos sobre las cartas y las estilizamos.
    Agregamos la clase btn-got.
      - Esta se utiliza para el carrito y las
      cards del market
    
    tmb cargamos mas productos con fetch
    a modo de prueba.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-let url="https://dummyjson.com/products?limit=3 "; /*Guardamos la url de donde vamos a consumir el recurso*/
+let url="https://dummyjson.com/products?limit=12 "; /*Guardamos la url de donde vamos a consumir el recurso*/
 const botonCarrito= document.getElementById("carritoIcon");
-const ito = document.getElementById("carrito")
+const carrito = document.getElementById("carrito")
 
 botonCarrito.onclick= ()=>{
     if(carrito.style.display=="flex"){
@@ -9,6 +9,7 @@ botonCarrito.onclick= ()=>{
         carrito.style.display="flex";
     }
 };
+
 fetch(url) /* Consumimos una API con productos*/
 .then(res => res.json()) 
 .then(producto=>mostrarProducto(producto.products))
@@ -22,14 +23,14 @@ function mostrarProducto(producto){
         card.className="card";
         card.style.width="90%"
         card.innerHTML = `
-        <div>
+        <div style="overflow=hidden">
             <img style="height: 265px;" src="${producto[i].thumbnail}" class="card-img-top" alt="...">
         </div>
         <div class="card-body">
           <h5 class="card-title">${producto[i].title}</h5>
           <p class="card-text">${producto[i].description} </p>
           <span class="precio">${producto[i].price}</span>  
-          <a href="#carrito" class="btn btn-primary botonCompra">Comprar</a>
+          <a href="#carrito" class="btn btn-primary btn-gott">Comprar</a>
         </div>
          `; 
         areaMarket.append(card);
